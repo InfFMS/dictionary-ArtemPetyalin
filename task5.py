79ba2240b50330ce3a722ffe0a26000687d4f21b
Петялин Артём Дмитриевич <a.petyalin@main.nsu.ru>
2025-04-28 10:12:06 +0700

    сделал 5

--- task5.py
+++ task5.py
@@ -5,4 +5,31 @@
 # Если введенного слова нет в словаре выводится сообщение "нет такого слова".
 # Используйте словари для словаря:)
 
-d = {}
+d = {'привет': 'hello', 'пока': 'goodbye', 'вода': 'water', 'пожалуйста': 'please'}
+d1 = {a:b for b, a in d.items()}
+a = input('Insert 1 for eng. Insert 2 for ru ')
+
+if a == 1:
+    word = input()
+
+    for i in list(d.keys()):
+
+        if word == i:
+            print(d[i])
+            break
+
+        if i == list(d.keys())[-1]:
+            print('Нет такого слова')
+
+
+else:
+    word = input()
+
+    for i in list(d.keys()):
+
+        if word == i:
+            print(d1[i])
+            break
+
+        if i == list(d1.keys())[-1]:
+            print('Нет такого слова')
