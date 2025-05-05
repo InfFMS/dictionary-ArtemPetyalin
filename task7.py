68b4ac0eb7527bf70cb7b40edce0fa6ae0f4b7a7
Петялин Артём Дмитриевич <a.petyalin@main.nsu.ru>
2025-05-05 10:40:36 +0700

    a

--- task7.py
+++ task7.py
@@ -6,3 +6,41 @@
 # Первый словарь: {'a': 100, 'b': 200, 'c':300}
 # Второй словарь: {'a': 300, 'b': 200, 'd':400}
 # Результат: {'a': 400, 'b': 400, 'd': 400, 'c': 300}
+
+d1 = {input(f'key {i + 1} '):int(input(f'value {i + 1} ')) for i in range(int(input('length 1 ')))}
+d2 = {input(f'key {j + 1} '):int(input(f'value {j + 1} ')) for j in range(int(input('length 2 ')))}
+keys3 = []
+values3 = []
+d3 = {}
+a = list(d1.keys())
+b = list(d2.keys())
+tse = list(d1.values())
+d = list(d2.values())
+b1 = b[:]
+a1 = a[:]
+tse1 = tse[:]
+d1 = d[:]
+
+for i in range(len(a)):
+
+    for j in range(len(b)):
+        print(i, j)
+
+        if a[i] == b[j]:
+            keys3.append(a[i])
+            values3.append(tse[i] + d[j])
+            a1.remove(a[i])
+            b1.remove(b[j])
+            tse1.remove(tse[i])
+            d1.remove(d[j])
+
+keys3 = keys3 + a1 + b1
+values3 = values3 + tse1 + d1
+
+for i in range(len(keys3)):
+    d3[keys3[i]] = values3[i]
+
+print(d3)
+
+
+
--- task8.py
+++ task8.py
@@ -1,3 +1,39 @@
 # Представьте, что вы шпионы)
 # Придумайте шифр, в котором буквы заменяются на какие-то символы
 # и реализуйте шифроватор и дешифроватор
+from typing import List
+
+sypher = {'a':'^', 'b': ')', 'c': '@', 'd':'_', 'e':','}
+desypher = {a:b for b, a in sypher.items()}
+
+a = int(input('1 or 2? '))
+
+
+if a == 1:      #шифроватор
+    l = ''
+
+    for i in input():
+
+        for j in list(sypher.keys()):
+
+            if i == j:
+                l += sypher[j]
+
+
+else:
+    l = ''
+
+    for i in input():
+
+        for j in list(desypher.keys()):
+
+            if i == j:
+                l += desypher[j]
+
+
+
+print(l)
+
+
+
+
